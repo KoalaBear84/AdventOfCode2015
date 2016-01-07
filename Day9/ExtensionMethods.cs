64340d2b9104639bc57d36c3625a09e1c11a9035
KoalaBear84 <animatrix84@gmail.com>
2016-01-07 14:00:19 +0100

    - Add Day 9

---
+++ Day9/ExtensionMethods.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day9
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> enumerable)
+        {
+            T[] array = enumerable as T[] ?? enumerable.ToArray();
+
+            long[] factorials = Enumerable.Range(0, array.Length + 1)
+                .Select(Factorial)
+                .ToArray();
+
+            for (long i = 0L; i < factorials[array.Length]; i++)
+            {
+                int[] sequence = GenerateSequence(i, array.Length - 1, factorials);
+
+                yield return GeneratePermutation(array, sequence);
+            }
+        }
+
+        static IEnumerable<T> GeneratePermutation<T>(T[] array, IReadOnlyList<int> sequence)
+        {
+            T[] clone = (T[])array.Clone();
+
+            for (int i = 0; i < clone.Length - 1; i++)
+            {
+                Swap(ref clone[i], ref clone[i + sequence[i]]);
+            }
+
+            return clone;
+        }
+
+        static int[] GenerateSequence(long number, int size, IReadOnlyList<long> factorials)
+        {
+            int[] sequence = new int[size];
+
+            for (int j = 0; j < sequence.Length; j++)
+            {
+                long facto = factorials[sequence.Length - j];
+
+                sequence[j] = (int)(number / facto);
+                number = (int)(number % facto);
+            }
+
+            return sequence;
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static long Factorial(int n)
+        {
+            long result = n;
+
+            for (int i = 1; i < n; i++)
+            {
+                result = result * i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day9
+{
+    static class ExtensionMethods2
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            return source.Where(element => seenKeys.Add(keySelector(element)));
+        }
+    }
+
+    class Program
+    {
+        static Regex Regex = new Regex(@"(?<Start>\w+) to (?<Stop>\w+) = (?<Distance>\d*)");
+        static List<Route> Routes = new List<Route>();
+
+        class Route
+        {
+            public string Start { get; set; }
+            public string Stop { get; set; }
+            public int Distance { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            foreach (string line in input)
+            {
+                Routes.AddRange(ParseRoute(line));
+            }
+
+            List<Route> star1 = CalculateRoute(Routes.OrderBy(r => r.Distance).ToList());
+
+            foreach (Route route in star1)
+            {
+                Console.WriteLine($"From {route.Start} to {route.Stop} = {route.Distance}");
+            }
+
+            Console.WriteLine($"Day 9 - Star 1, Answer: {star1.Sum(r => r.Distance)}");
+
+            List<Route> star2 = CalculateRoute(Routes.OrderByDescending(r => r.Distance).ToList(), false);
+
+            foreach (Route route in star2)
+            {
+                Console.WriteLine($"From {route.Start} to {route.Stop} = {route.Distance}");
+            }
+
+            Console.WriteLine($"Day 9 - Star 2, Answer: {star2.Sum(r => r.Distance)}");
+
+            Console.ReadKey();
+        }
+
+        static List<Route> CalculateRoute(List<Route> routes, bool shortest = true)
+        {
+            // Get a list of all distinct cities
+            List<string> cities = routes.DistinctBy(r => r.Start).Select(r => r.Start).ToList();
+
+            // Get all possible combinations
+            List<IEnumerable<string>> cityPermutations = cities.GetPermutations().ToList();
+
+            List<List<Route>> allRoutes = new List<List<Route>>();
+
+            // Go through all possible combinations
+            foreach (IEnumerable<string> cityPermutation in cityPermutations)
+            {
+                List<Route> currentRoute = new List<Route>();
+                string currentCity = cityPermutation.Take(1).First();
+
+                // Go to all cities
+                foreach (string nextCity in cityPermutation.Skip(1))
+                {
+                    currentRoute.Add(routes.First(r => r.Start == currentCity && r.Stop == nextCity));
+                    currentCity = nextCity;
+                }
+
+                allRoutes.Add(currentRoute);
+            }
+
+            if (shortest)
+            {
+                return allRoutes.OrderBy(route => route.Sum(r => r.Distance)).First();
+            } else
+            {
+                return allRoutes.OrderByDescending(route => route.Sum(r => r.Distance)).First();
+            }
+        }
+
+        static List<Route> ParseRoute(string input)
+        {
+            Match regexMatch = Regex.Match(input);
+
+            List<Route> routes = new List<Route>();
+
+            routes.Add(new Route()
+            {
+                Start = regexMatch.Groups["Start"].Value,
+                Stop = regexMatch.Groups["Stop"].Value,
+                Distance = int.Parse(regexMatch.Groups["Distance"].Value)
+            });
+
+            routes.Add(new Route()
+            {
+                Start = regexMatch.Groups["Stop"].Value,
+                Stop = regexMatch.Groups["Start"].Value,
+                Distance = int.Parse(regexMatch.Groups["Distance"].Value)
+            });
+
+            return routes;
+        }
+    }
+}
