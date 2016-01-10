5e96974a7d8eeb3f38d680526b86349da74910db
KoalaBear84 <animatrix84@gmail.com>
2016-01-10 11:42:47 +0100

    - Add Day 11

---
+++ Day11/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    public static class ExtensionMethods
+    {
+        public static string ReplaceAtIndex(this string text, int index, char c)
+        {
+            var stringBuilder = new StringBuilder(text);
+            stringBuilder[index] = c;
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day11
+{
+    class Program
+    {
+        static char[] InvalidCharacters = { 'i', 'o', 'l' };
+
+        static void Main(string[] args)
+        {
+            string star1 = CalculateNewPassword("cqjxjnds");
+
+            Console.WriteLine("Day 11 - Star 1, Answer: {0}", star1);
+
+            string star2 = CalculateNewPassword(star1);
+
+            Console.WriteLine("Day 11 - Star 2, Answer: {0}", star2);
+
+            Console.ReadKey();
+        }
+
+        static string CalculateNewPassword(string currentPassword)
+        {
+            string nextPassword = currentPassword;
+            int iterations = 0;
+
+            do
+            {
+                iterations++;
+                nextPassword = IncreasePassword(nextPassword);
+            } while (
+                !HasThreeIncreasingCharacters(nextPassword) ||
+                !HasNoInvalidCharacters(nextPassword) ||
+                !HasTwoPairs(nextPassword)
+            );
+
+            return nextPassword;
+        }
+
+        static string IncreasePassword(string password)
+        {
+            int charPosition = password.Length - 1;
+
+            char nextCharacter;
+            bool nextPosition = true;
+
+            do
+            {
+                nextCharacter = IncreaseChar(password[charPosition]);
+
+                password = password.ReplaceAtIndex(charPosition, nextCharacter);
+
+                nextPosition = (nextCharacter == 'a');
+
+                if (nextPosition)
+                {
+                    charPosition--;
+                }
+            } while (nextPosition);
+
+            return password;
+        }
+
+        static char IncreaseChar(char character)
+        {
+            return (char)((((character - 97) + 1) % 26) + 97);
+        }
+
+        static bool HasThreeIncreasingCharacters(string password)
+        {
+            for (int i = 0; i < password.Length - 2; i++)
+            {
+                if ((password[i] + 2) == (password[i + 1] + 1) && (password[i + 1] + 1) == (password[i + 2]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool HasNoInvalidCharacters(string password)
+        {
+            return !password.Any(c => InvalidCharacters.Contains(c));
+        }
+
+        static List<string> GetPairs()
+        {
+            List<string> pairs = new List<string>();
+
+            for (int i = 97; i < 97 + 26; i++)
+            {
+                pairs.Add(((char)i).ToString() + ((char)i).ToString());
+            }
+
+            return pairs;
+        }
+
+        static bool HasTwoPairs(string password)
+        {
+            List<string> pairs = GetPairs();
+
+            for (int i = 0; i < password.Length - 1; i++)
+            {
+                string pair = string.Concat(password[i], password[i + 1]);
+
+                if (pairs.Contains(pair))
+                {
+                    pairs.Remove(pair);
+                }
+
+                if (pairs.Count == 24)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
